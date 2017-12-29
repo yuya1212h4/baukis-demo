c71c8aea4fcde7dfd6bb16c958e0e7dc05ec79a0
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-12-29 16:15:46 +0900

    create authorize method on staff/controllers

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,4 +1,6 @@
 class Staff::Base < ApplicationController
+  before_action :authorize
+
   private
   def current_staff_member
     if session[:staff_member_id]
@@ -7,4 +9,11 @@ class Staff::Base < ApplicationController
   end
 
   helper_method :current_staff_member # メソッドをテンプレートの中でも使用出来るようにしている
+
+  def authorize
+    unless current_staff_member
+      flash.alert = '職員としてログインをして下さい。'
+      redirect_to :staff_login
+    end
+  end
 end
